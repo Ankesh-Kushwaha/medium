ae527f731a1784429a5c1b1788071d34d32de98e
ankeshkushwaha <ankeshkush9651@gmail.com>
2025-04-27 11:18:03 +0530

    build the project for deploy

--- frontend/medium-frontend/src/pages/Home.tsx
+++ frontend/medium-frontend/src/pages/Home.tsx
@@ -5,7 +5,6 @@ const Home = () => {
        <div >
         <MainHeader/>
       <img className='w-screen h-screen object-cover' src="https://img.freepik.com/premium-photo/girl-write-open-white-book-accounting-minimal-clean-light-blue-desk-with-laptop-accessories-copy-space-flat-lay-top-view-mock-up_315337-7591.jpg?w=1380" alt="" />
-        Home
        </div>
   )
 }
